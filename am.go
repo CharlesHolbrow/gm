833f648afd9b8c8042e2d7bead1257969fb493ca
Charles Holbrow <charlesholbrow@gmail.com>
2018-02-01 03:37:54 -0500

    finish rename midiLogger

--- am.go
+++ am.go
@@ -22,7 +22,7 @@ func main() {
 	}
 
 	fmt.Printf("Listening for MIDI on %d - %v\n", midiInputDeviceID, *portmidi.Info(midiInputDeviceID))
-	myMidiHandler := &midiHandler{}
+	myMidiHandler := &midiLogger{}
 	ms, err := MakeMidiStream(midiInputDeviceID, myMidiHandler)
 	if err != nil {
 		log.Panicln("Error Making Midi Stream", err)
